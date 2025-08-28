7f03b5aa5692d19ab0d5ce03d312a2f9ebb3f6fc
alpharabbit9 <alpharabbit74@gmail.com>
2025-08-29 03:12:12 +0600

    Fix poster import issue

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -9,38 +9,7 @@ const services = [
         accent: "bg-[#B9FF66]",
         dark: false,
         textBg: "bg-[#B9FF66]",
-        illustration: (
-            <svg
-                width="210"
-                height="170"
-                viewBox="0 0 210 170"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-            >
-                <rect y="2" width="210" height="166.047" fill="url(#pattern0_1_624)" />
-                <defs>
-                    <pattern
-                        id="pattern0_1_624"
-                        patternContentUnits="objectBoundingBox"
-                        width="1"
-                        height="1"
-                    >
-                        <use
-                            xlinkHref="#image0_1_624"
-                            transform="matrix(0.00581395 0 0 0.00735294 -0.244186 -0.441176)"
-                        />
-                    </pattern>
-                    <image
-                        id="image0_1_624"
-                        width="256"
-                        height="256"
-                        preserveAspectRatio="none"
-                        xlinkHref="data:image/png;base64,iVBORw0K..."
-                    />
-                </defs>
-            </svg>
-        ),
+       illustration: <></>,
     },
     {
         title: "Pay-per-click advertising",
